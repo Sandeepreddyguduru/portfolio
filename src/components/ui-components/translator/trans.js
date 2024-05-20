3848ffa8777afcfeba0eda77a0b4d535cf8ed44e
Sandeepreddyguduru <reddy6960@gmail.com>
2024-05-21 01:11:17 +0530

    Updates

--- src/components/ui-components/translator/trans.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useTranslation } from "react-i18next";
-
-const Trans = ({Translate}) => {
-  const { t } = useTranslation();
-  return <>{t(Translate)}</>;
-};
-export default Trans;
